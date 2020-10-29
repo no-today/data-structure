62bb1989492402fdb8d20fbfa69db1f9a8da5dd1
notoday <no.today@outlook.com>
2020-10-29 21:33:07 +0800

    Update

--- src/main/java/data/structure/tree/AVLTree.java
+++ src/main/java/data/structure/tree/AVLTree.java
@@ -3,11 +3,17 @@ package data.structure.tree;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * 在计算机科学中，AVL树是最早被发明的自平衡二叉查找树。在AVL树中，任一节点对应的两棵子树的最大高度差为1，因此它也被称为高度平衡树。
+ * 查找、插入和删除在平均和最坏情况下的时间复杂度都是 O(logn)。增加和删除元素的操作则可能需要借由一次或多次树旋转，以实现树的重新平衡。
+ * AVL树得名于它的发明者G. M. Adelson-Velsky和Evgenii Landis，他们在1962年的论文《An algorithm for the organization of information》中公开了这一数据结构。
+ * <p>
+ * 节点的平衡因子是它的左子树的高度减去它的右子树的高度（有时相反）。带有平衡因子1、0或 -1的节点被认为是平衡的。带有平衡因子 -2或2的节点被认为是不平衡的，
+ * 并需要重新平衡这个树。平衡因子可以直接存储在每个节点中，或从可能存储在节点中的子树高度计算出来。
+ * <p>
  * 平衡二叉树
  * <p>
  * 对于任意一个节点，左子树和右子数的高度差不超过 1
  * <p>
- * 平衡
  */
 public class AVLTree<E extends Comparable<E>> {
 
